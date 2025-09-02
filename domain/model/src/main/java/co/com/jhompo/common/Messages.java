2ecf7f220d981964ec06a8160141e3a19b2f563a
Jhonatan Caraballo Ricardo <jhonatan.caraballo@neoris.com>
2025-09-02 13:48:05 -0500

    refactoring Messages

--- domain/model/src/main/java/co/com/jhompo/common/Messages.java
+++ domain/model/src/main/java/co/com/jhompo/common/Messages.java
@@ -1,4 +1,4 @@
-package co.com.jhompo.util;
+package co.com.jhompo.common;
 
 public final class Messages {
 
@@ -59,6 +59,8 @@ public final class Messages {
     // ============================================
     public static final class LOAN_APPLICATION {
         // Éxito
+        public static final String TITLE = "Solicitudes de Crédito";
+        public static final String DESCRIPTION= "Gestión de solicitudes de préstamo";
         public static final String CREATED_SUCCESS = "Solicitud de préstamo creada exitosamente";
         public static final String UPDATED_SUCCESS = "Solicitud de préstamo actualizada exitosamente";
         public static final String DELETED_SUCCESS = "Solicitud de préstamo eliminada exitosamente";
@@ -67,14 +69,23 @@ public final class Messages {
         public static final String REJECTED_SUCCESS = "Solicitud de préstamo rechazada";
         public static final String CANCELLED_SUCCESS = "Solicitud de préstamo cancelada";
 
+        public static final String DESCRIPTION_CREATE = "Crear nueva solicitud de crédito";
+        public static final String DESCRIPTION_UPDATE = "Actualizar solicitud de crédito";
+        public static final String DESCRIPTION_DELETE=  "Eliminar solicitud de crédito";
+        public static final String DESCRIPTION_GET_ALL=  "Listar todas las solicitudes de crédito";
+        public static final String DESCRIPTION_FIND_BY_ID = "Buscar solicitud por ID";
+        public static final String DESCRIPTION_GET_SUMMARY =  "Listar solicitudes por estado paginado y filtrado";
+
+        public static final String EMAIL_NOT_FOUND ="El  email ingresado no existe en el sistema de autenticación";
         // Errores
         public static final String NOT_FOUND = "Solicitud de préstamo no encontrada";
         public static final String CREATION_FAILED = "Error al crear la solicitud de préstamo";
         public static final String UPDATE_FAILED = "Error al actualizar la solicitud de préstamo";
         public static final String DELETE_FAILED = "Error al eliminar la solicitud de préstamo";
-        public static final String INVALID_STATUS_TRANSITION = "Transición de estado inválida para la solicitud";
+        public static final String LIST_ERROR = "Error al listar solicitudes";
         public static final String INSUFFICIENT_FUNDS = "El monto solicitado excede el límite de aprobación";
         public static final String DUPLICATE_ENTRY = "Ya existe una solicitud de préstamo con esta información";
+        public static final String CANNOT_CREATE_REQUEST_FOR_OTHER_USER  = "No puede crear una solicitud a nombre de otro usuario";
 
         // Validaciones
         public static final String AMOUNT_REQUIRED = "El monto es requerido y debe ser mayor a 0";
@@ -87,10 +98,28 @@ public final class Messages {
     // MENSAJES DE ESTADOS
     // ============================================
     public static final class STATUS {
+        public static final String TITLE = "Estados";
+        public static final String DESCRIPTION= "Gestión de estados del sistema";
+        public static final String PENDING_REVIEW="PENDIENTE_REVISION";
+        public static final String DESCRIPTION_PENDING="Pendiente de revisión por un analista";
         public static final String CREATED_SUCCESS = "Estado creado exitosamente";
+        public static final String UPDATED_SUCCESS = "Estado actualizado exitosamente";
+        public static final String DELETED_SUCCESS = "Estado eliminado exitosamente";
         public static final String FOUND_SUCCESS = "Estado(s) encontrado(s) exitosamente";
         public static final String NOT_FOUND = "Estado no encontrado";
         public static final String ALREADY_EXISTS = "El estado ya existe";
+
+
+        public static final String CREATION_FAILED = "Error al crear estado de credito";
+        public static final String UPDATE_FAILED = "Error al actualizar estado de credito";
+        public static final String DELETE_FAILED = "Error al eliminar estado de credito";
+
+        public static final String DESCRIPTION_CREATE = "Crear nuevo estado";
+        public static final String DESCRIPTION_UPDATE = "Actualizar estado";
+        public static final String DESCRIPTION_DELETE=  "Eliminar estado";
+        public static final String DESCRIPTION_GET_ALL=  "Listar todas los estados";
+        public static final String DESCRIPTION_FIND_BY_ID = "Buscar estado por ID";
+        public static final String DESCRIPTION_GET_SUMMARY = "Listar estado paginado y filtrado";
 
         private STATUS() {}
     }
@@ -99,10 +128,26 @@ public final class Messages {
     // MENSAJES DE TIPOS DE APLICACIÓN
     // ============================================
     public static final class APPLICATION_TYPE {
-        public static final String CREATED_SUCCESS = "Tipo de aplicación creado exitosamente";
-        public static final String FOUND_SUCCESS = "Tipo(s) de aplicación encontrado(s) exitosamente";
-        public static final String NOT_FOUND = "Tipo de aplicación no encontrado";
-        public static final String ALREADY_EXISTS = "El tipo de aplicación ya existe";
+        public static final String TITLE = "Tipos de Crédito";
+        public static final String DESCRIPTION= "Gestión de Tipos de Crédito del sistema";
+        public static final String CREATED_SUCCESS = "Tipo de credito creado exitosamente";
+        public static final String UPDATED_SUCCESS = "Tipo de credito actualizado exitosamente";
+        public static final String DELETED_SUCCESS = "Tipo de credito eliminado exitosamente";
+        public static final String FOUND_SUCCESS = "Tipo(s) de credito encontrado(s) exitosamente";
+        public static final String NOT_FOUND = "Tipo de credito no encontrado";
+        public static final String ALREADY_EXISTS = "El tipo de credito ya existe";
+
+
+        public static final String CREATION_FAILED = "Error al crear tipo de credito";
+        public static final String UPDATE_FAILED = "Error al actualizar tipo de credito";
+        public static final String DELETE_FAILED = "Error al eliminar tipo de credito";
+
+        public static final String DESCRIPTION_CREATE = "Crear nuevo tipo de credito";
+        public static final String DESCRIPTION_UPDATE = "Actualizar tipo de credito";
+        public static final String DESCRIPTION_DELETE=  "Eliminar tipo de credito";
+        public static final String DESCRIPTION_GET_ALL=  "Listar todas los tipos de creditos";
+        public static final String DESCRIPTION_FIND_BY_ID = "Buscar tipo de credito por ID";
+        public static final String DESCRIPTION_GET_SUMMARY =  "Listar tipo de credito paginado y filtrado";
 
         private APPLICATION_TYPE() {}
     }
@@ -136,14 +181,32 @@ public final class Messages {
         public static final String CACHE_CLEARED = "Caché limpiado exitosamente";
 
         // Errores
+        public static final String OPERATION_ERROR= "Error, operación no realizada exitosamente";
+
+        public static final String DATABASE_ERROR = "Error con la base de datos";
+        public static final String DUPLICATE_KEY = "Duplicate Key";
+        public static final String DATA_INTEGRITY_VIOLATION = "Data integrity violation";
+        public static final String DATABASE_CONSTRAINT_VIOLATION = "El registro ya existe o viola una restricción de la base de datos";
+
         public static final String INTERNAL_ERROR = "Error interno del servidor";
-        public static final String DATABASE_ERROR = "Error de conexión a la base de datos";
         public static final String NETWORK_ERROR = "Error de conexión de red";
-        public static final String SERVICE_UNAVAILABLE = "Servicio no disponible";
+        public static final String SERVICE_UNAVAILABLE = "Servicio no disponible, Intente de nuevo más tarde";
         public static final String TIMEOUT_ERROR = "Tiempo de espera agotado";
         public static final String VALIDATION_ERROR = "Error de validación";
         public static final String SERIALIZATION_ERROR = "Error de serialización";
         public static final String DESERIALIZATION_ERROR = "Error de deserialización";
+
+
+        public static final String EXTERNAL_SERVICE_COMMUNICATION_ERROR = "Ocurrió un error al comunicarse con un servicio externo";
+        public static final String INVALID_ARGUMENT = "Argumento invalido";
+        public static final String NULL_POINTER_EXCEPTION = "Null Pointer Exception";
+        public static final String ILLEGAL_STATE = "Illegal State";
+        public static final String DUPLICATE_LOAN_REQUEST = "Ya existe una solicitud con este email para el tipo de préstamo seleccionado";
+        public static final String UNEXPECTED_NULL_VALUE = "Se encontró un valor nulo inesperado";
+
+        public static final String CREATION_FAILED = "Error al crear la operacion";
+        public static final String UPDATE_FAILED = "Error al actualizar la operacion";
+        public static final String DELETE_FAILED = "Error al eliminar la operacion";
 
         // Validaciones generales
         public static final String REQUIRED_FIELD = "Campo requerido";
--- infrastructure/entry-points/reactive-web/src/main/java/co/com/jhompo/api/ApplicationTypeController.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/jhompo/api/ApplicationTypeController.java
@@ -3,27 +3,33 @@ package co.com.jhompo.api;
 import co.com.jhompo.api.dtos.ApplicationTypeDTO;
 import co.com.jhompo.api.mapper.ApplicationTypeMapper;
 import co.com.jhompo.usecase.applicationtype.ApplicationTypeUseCase;
+import io.netty.handler.logging.LogLevel;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import static co.com.jhompo.common.Messages.*;
+
+@Slf4j
 @RestController
 @RequestMapping("/api/v1/loantype")
 @RequiredArgsConstructor
-@Tag(name = "Tipos de Crédito", description = "Gestión de tipos de solicitud de crédito")
+@Tag(name = APPLICATION_TYPE.TITLE, description = APPLICATION_TYPE.DESCRIPTION)
 public class ApplicationTypeController {
 
     private final ApplicationTypeUseCase service;
     private final ApplicationTypeMapper mapper;
 
-    @Operation(summary = "Crear nuevo tipo de crédito")
+    @Operation(summary = APPLICATION_TYPE.DESCRIPTION_CREATE)
     @PostMapping
     public Mono<ResponseEntity<ApplicationTypeDTO>> createApplicationType(@RequestBody ApplicationTypeDTO applicationTypeDTO) {
+        log.info(APPLICATION_TYPE.DESCRIPTION_CREATE);
         return Mono.just(applicationTypeDTO)
                 .map(mapper::toEntity)
                 .flatMap(service::create)
@@ -31,9 +37,11 @@ public class ApplicationTypeController {
                 .map(created -> new ResponseEntity<>(created, HttpStatus.CREATED));
     }
 
-    @Operation(summary = "Listar todos los tipos de crédito")
+    @Operation(summary = APPLICATION_TYPE.DESCRIPTION_GET_ALL)
     @GetMapping
     public Flux<ApplicationTypeDTO> getAll() {
+        log.info(APPLICATION_TYPE.DESCRIPTION_GET_ALL);
+
         return service.getAllStatuses()
                 .map(mapper::toDto);
     }
